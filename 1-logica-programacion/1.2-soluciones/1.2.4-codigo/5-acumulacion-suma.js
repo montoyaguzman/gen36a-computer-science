199239c76b6cc5bbb375d95e93649dab5db744cc
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-08-12 19:14:56 -0600

    feat: debugger, for vs while

--- 1-logica-programacion/1.2-soluciones/1.2.4-codigo/5-acumulacion-suma.js
+++ 1-logica-programacion/1.2-soluciones/1.2.4-codigo/5-acumulacion-suma.js
@@ -1,15 +1,29 @@
 let numbers = [1, 4, 7, 9, 3, 3, 5, 90];
 let total = 0;
 
-// for (inicio; condicion (repetir ciclo); razonCambio )
-for (let i = 0; i < numbers.length; i++) {
+// for (inicio; condicion (repetir ciclo); razonCambio)
+// for (let i = 0; i < numbers.length; i++) {
+//     debugger;
+//     if (i === 0) {
+//         total = total + numbers[0];
+//     }
+//     if (i % 3 !== 0) {
+//         total = total + numbers[i];
+//         // console.log(numbers[i]);
+//     }
+// }
+
+
+let i = 0; // inicio
+while (i < numbers.length) { // condicion
     if (i === 0) {
         total = total + numbers[0];
     }
     if (i % 3 !== 0) {
         total = total + numbers[i];
-        // console.log(numbers[i])
+        // console.log(numbers[i]);
     }
+    i++; // razonDeCambio
 }
 
 console.log('total: ', total);
