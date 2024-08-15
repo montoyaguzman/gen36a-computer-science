ca7334de5f2ad65a71dd5fdaa67ea0cb0dc62e72
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-08-14 19:34:08 -0600

    feat: for in y for of, ejemplos y reto

--- 2-ecmascript/js/4.fors-in-of.js
+++ 2-ecmascript/js/4.fors-in-of.js
@@ -10,7 +10,9 @@ const colors = [
 
 const senseiMontoya = {
     name: 'jose',
-    students: ['gabriel', 'luis']
+    students: ['gabriel', 'luis'],
+    age: 30
+    // prop: value
 }
 
 // senseiMontoya.name // jose
@@ -26,16 +28,62 @@ const fruits = [
 // ES5
 // Mostrar el mensaje "La NAME cuesta PRICE y SI/NO esta en stock"
 for (let i = 0; i < fruits.length; i++) {
-    console.log(fruits[i].name);
+    const fruit = fruits[i]; // iterador arreglo[pos]
+
+    // operador ternario
+    // condicion ? resultadoCondicionTrue : resultadoCondicionFalse
+    const finalText = fruit.isStock ? 'está en stock' : 'no está en stock'
+
+    // let finalText = '';
+    // if (fruit.isStock) {
+    //     finalText = 'está en stock';
+    // } else {
+    //     finalText = 'no está en stock';
+    // }
+    console.log(`La ${fruit.name} cuesta ${fruit.price} y ${finalText}`);
+
 }
 
-// ES6: FOR OF
+// ES6: FOR OF => Es para iterar arreglos
 // Mostrar el mensaje "La NAME cuesta PRICE y SI/NO esta en stock"
+// 
+for (const fruit of fruits) {
+    console.log(fruit);
+    const finalText = fruit.isStock ? 'está en stock' : 'no está en stock'
+    console.log(`La ${fruit.name} cuesta ${fruit.price} y ${finalText}`);
+}
 
-
-// ES6: FOR IN
+// ES6: FOR IN => No se puede hacer ya que for in es para objetos
 // Mostrar el mensaje "La NAME cuesta PRICE y SI/NO esta en stock"
+for (const prop in senseiMontoya) {
+    // senseiMontoya.name
+    // senseiMontoya.age
+    console.log(senseiMontoya[prop]);
+    // fruits[fruit]
+}
 
 
 // Con un ciclo de ES5 (for y while) y uno de ES6 (for in o el for of)
 // Mostrar el mensaje "La NAME cuesta PRICE y SI/NO esta en stock y lo venden en TIENDA1, TIENDA2, ... TIENDAN"
+
+// for => programacion imperativa
+// for of y for in => programacion declrativa
+
+
+// ES6: FOR IN
+// Mostrar el mensaje "La NAME cuesta PRICE y SI/NO esta en stock y lo venden en TIENDA1, TIENDA2, ... TIENDAN"
+for (let fruit of fruits) {
+    // Mostrar el mensaje "La NAME cuesta PRICE y SI/NO esta en stock y lo venden en TIENDA1, TIENDA2, ... TIENDAN"
+    const message = fruit.isStock ? 'si' : 'no'
+    let storesMessage = '';
+    for (let prop in fruit) {
+        if (prop === 'stores') {
+            // console.log(fruit[prop]); // stores: ['buelmer', 'badiga eurrira', 'chadreui'] => 'buelmer', 'badiga eurrira', 'chadreui'
+            for (let store of fruit[prop]) {
+                storesMessage += `${store}`;
+                //  storesMessage += fruit[prop].length - 1 === i ? '.' : ',' como obtener la i?????
+            }
+        }
+    }
+    console.log(`La ${fruit.name} cuesta ${fruit.price} y ${message} esta en stock y lo venden en ${storesMessage}`);
+}
