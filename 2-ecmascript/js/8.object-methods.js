ff6f122fea18e3b2d1bcb643baa8a005f4e6d14c
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-08-14 21:24:53 -0600

    feat: object methods y valor por defecto

---
+++ 2-ecmascript/js/8.object-methods.js
@@ -0,0 +1,11 @@
+const superhero = {
+    id: 1,
+    name: 'peter',
+    app: 'parker',
+    alias: 'spiderman'
+};
+
+console.log('Object.keys:', Object.keys(superhero));
+console.log('Object.values:', Object.values(superhero));
+console.log('Object.entries:', Object.entries(superhero));
+console.log('Object.length:', Object.keys(superhero).length);
---
+++ 2-ecmascript/js/9.valor-por-defecto.js
@@ -0,0 +1,11 @@
+const harryPotter = {
+    species: "human",
+    // gender: "male",
+    phone: '1122334455'
+};
+
+const phone = harryPotter.phone || '';
+const genre = harryPotter.genre || 'X';
+
+console.log(phone);
+console.log(genre);
