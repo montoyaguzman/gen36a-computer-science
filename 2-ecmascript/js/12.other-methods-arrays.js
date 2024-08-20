47533de4e27a358f705dc11daac4b2b527bf4a41
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-08-19 19:34:00 -0600

    feat: metodos para arreglos

---
+++ 2-ecmascript/js/12.other-methods-arrays.js
@@ -0,0 +1,10 @@
+const array = [100, 90, 110, 101, 12, 70];
+
+
+array.push(88);
+array.splice(3, 1); // pos, cantidadAEliminar
+array.includes(100);
+
+array.find();
+
+// revisar el resto de metodos de arreglo en la ppt
